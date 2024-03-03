97ad92f523e8e4fead112d07e6dce96ca262ca50
Anton Kirzyk <kirzyka@gmail.com>
2024-03-03 20:45:54 +0000

    task # 1

--- time_series_visualizer.py
+++ time_series_visualizer.py
@@ -1,25 +1,36 @@
 import matplotlib.pyplot as plt
+from matplotlib.ticker import MaxNLocator
 import pandas as pd
 import seaborn as sns
 from pandas.plotting import register_matplotlib_converters
 register_matplotlib_converters()
 
 # Import data (Make sure to parse dates. Consider setting index column to 'date'.)
-df = None
+df = pd.read_csv("./fcc-forum-pageviews.csv", header=0)
+df.set_index('date', inplace=True)
 
 # Clean data
-df = None
+df = df[(df['value'] >= df['value'].quantile(0.025)) &
+        (df['value'] <= df['value'].quantile(0.975))]
 
+print(df)
 
 def draw_line_plot():
     # Draw line plot
-
-
-
-
+    fig, ax = plt.subplots(figsize=(10, 6))
+    ax.plot(df.index, df['value'], color='blue', linewidth=1)
+    
+    ax.set_xlabel('Date')
+    ax.set_ylabel('Page Views')
+    ax.set_title('Page Views on freeCodeCamp.org Forum (2016-05-09 to 2019-12-03)')
+    plt.xticks(rotation=45, ha='right')    
+    ax.xaxis.set_major_locator(MaxNLocator(5))
 
     # Save image and return fig (don't change this part)
-    fig.savefig('line_plot.png')
+    plt.tight_layout()
+    plt.savefig('line_plot.png')
+    plt.show()
+
     return fig
 
 def draw_bar_plot():
